677dccf3c6df7912dc1de1a089b5e858af5eb074
Luigi <l-palermo@hotmail.it>
2019-05-16 12:43:58 +0000

    Added hash and improved readability

--- directory.rb
+++ directory.rb
@@ -1,23 +1,23 @@
 # first we print the list of students
 students = [
- "Dr. Hannibal Lecter",
- "Darth Vader",
- "Nurse Ratched",
- "Michael Corleone",
- "Alex DeLarge",
- "The Wicked Wich of the West",
- "Terminator",
- "Freddy Krueger",
- "The Joker",
- "Joffrey Baratheon",
- "Norman Bates"
+ ["Dr. Hannibal Lecter", :november],
+ ["Darth Vader", :november],
+ ["Nurse Ratched", :november],
+ ["Michael Corleone", :november],
+ ["Alex DeLarge", :november],
+ ["The Wicked Wich of the West", :november],
+ ["Terminator", :november],
+ ["Freddy Krueger", :november],
+ ["The Joker", :november],
+ ["Joffrey Baratheon", :november],
+ ["Norman Bates", :november]
 ]
 def print_header
   puts "The students of Villains Academy"
   puts "--------------------------------"
 end
 def print(names)
-names.each do |student| puts student end
+names.each do |student| puts "#{student[0]} (#{student[1]} cohort)" end
 end
 def print_footer(names)
 puts "Overall, we have #{names.count} great students"
