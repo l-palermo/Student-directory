a301085a6ae049c63c37c48321974a64a21c87ce
Luigi <l-palermo@hotmail.it>
2019-05-20 19:36:25 +0000

    Added user input

--- directory.rb
+++ directory.rb
@@ -1,28 +1,52 @@
 # first we print the list of students
 students = [
- ["Dr. Hannibal Lecter", :november],
- ["Darth Vader", :november],
- ["Nurse Ratched", :november],
- ["Michael Corleone", :november],
- ["Alex DeLarge", :november],
- ["The Wicked Wich of the West", :november],
- ["Terminator", :november],
- ["Freddy Krueger", :november],
- ["The Joker", :november],
- ["Joffrey Baratheon", :november],
- ["Norman Bates", :november]
+ {name: "Dr. Hannibal Lecter", cohort: :november},
+ {name: "Darth Vader", cohort: :november},
+ {name: "Nurse Ratched", cohort: :november},
+ {name: "Michael Corleone", cohort: :november},
+ {name: "Alex DeLarge", cohort: :november},
+ {name: "The Wicked Wich of the West", cohort: :november},
+ {name: "Terminator", cohort: :november},
+ {name: "Freddy Krueger", cohort: :november},
+ {name: "The Joker", cohort: :november},
+ {name: "Joffrey Baratheon", cohort: :november},
+ {name: "Norman Bates", cohort: :november}
 ]
+
 def print_header
   puts "The students of Villains Academy"
   puts "--------------------------------"
 end
+
 def print(names)
-names.each do |student| puts "#{student[0]} (#{student[1]} cohort)" end
+names.each do |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" end
 end
+
 def print_footer(names)
 puts "Overall, we have #{names.count} great students"
 end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeate this code
+  while !name.empty? do 
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # returns the array of students
+  students
+end 
+
 # nothing happens untill we call the methods
+students = input_students
 print_header
 print(students)
-print_footer(students) 
+print_footer(students)
