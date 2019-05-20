ebba08a2fb43a495c28f559596e397bb3af645dd
Luigi <l-palermo@hotmail.it>
2019-05-20 19:40:39 +0000

    Cp. 8 - Exercise 1

--- directory.rb
+++ directory.rb
@@ -1,17 +1,6 @@
-# first we print the list of students
-students = [
- {name: "Dr. Hannibal Lecter", cohort: :november},
- {name: "Darth Vader", cohort: :november},
- {name: "Nurse Ratched", cohort: :november},
- {name: "Michael Corleone", cohort: :november},
- {name: "Alex DeLarge", cohort: :november},
- {name: "The Wicked Wich of the West", cohort: :november},
- {name: "Terminator", cohort: :november},
- {name: "Freddy Krueger", cohort: :november},
- {name: "The Joker", cohort: :november},
- {name: "Joffrey Baratheon", cohort: :november},
- {name: "Norman Bates", cohort: :november}
-]
+# #################################################
+# Cp. 8 - Exercise 1
+# #################################################
 
 def print_header
   puts "The students of Villains Academy"
@@ -19,7 +8,9 @@ def print_header
 end
 
 def print(names)
-names.each do |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" end
+  names.each_with_index do |student, index| 
+    puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)" 
+  end
 end
 
 def print_footer(names)
